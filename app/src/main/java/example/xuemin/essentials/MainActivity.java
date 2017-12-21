ac90d317d8bd0eb5a57090264702e602e603ae0a
xuemin <xuemin@xuemindeMacBook-Pro.local>
2017-12-21 11:08:04 +0800

    first commit

---
+++ app/src/main/java/example/xuemin/essentials/MainActivity.java
@@ -0,0 +1,15 @@
+package example.xuemin.essentials;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by xuemin on 2017/12/21.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
